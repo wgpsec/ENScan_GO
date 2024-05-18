ca2965c97976f4bec622bc5752168dde1bae4199
keac <admin@wgpsec.org>
2024-05-18 21:52:39 +0800

    优化错误判断

--- enscan.go
+++ enscan.go
@@ -20,12 +20,14 @@ func main() {
 				gologger.Error().Msgf("任务未完成退出，自动保存过程文件！")
 				enDataList := make(map[string][]map[string]string)
 				close(runner.EnCh)
-				for ch := range runner.EnCh {
-					utils.MergeMap(ch, enDataList)
-				}
-				err := common.OutFileByEnInfo(enDataList, "意外退出保存文件", "xlsx", "outs")
-				if err != nil {
-					gologger.Error().Msgf(err.Error())
+				if len(runner.EnCh) > 0 {
+					for ch := range runner.EnCh {
+						utils.MergeMap(ch, enDataList)
+					}
+					err := common.OutFileByEnInfo(enDataList, "意外退出保存文件", "xlsx", "outs")
+					if err != nil {
+						gologger.Error().Msgf(err.Error())
+					}
 				}
 				log.Fatal("exit.by.signal")
 			}
